6e74d9a73f5372323f3e762704f21d7407d684ea
Tanimismat <tanim.ismat@gmail.com>
2022-05-15 12:22:34 +0600

    slots displayed dynamically

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -7,9 +7,17 @@ const Service = ({ service }) => {
         <div className="card w-96 bg-base-100 shadow-xl">
             <div className="card-body">
                 <h2 className="card-title">{name}</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
+                <p>
+                    {
+                        slots.length ?
+                            <span>{slots[0]}</span>
+                            :
+                            <span>No slots available</span>
+                    }
+                </p>
+                <p>{slots.length} {slots.length < 1 ? 'space' : 'spaces'} available</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button disabled={slots.length === 0} className="btn btn-primary">Book appointment</button>
                 </div>
             </div>
         </div>
